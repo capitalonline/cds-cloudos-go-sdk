5da497c305095fe7d6b4ba81121eb9d90232c30f
yulong.deng <yulong.deng@capitalonline.net>
2025-09-09 16:32:53 +0800

    cluster example

--- examples/eks/cluster.go
+++ examples/eks/cluster.go
@@ -118,56 +118,53 @@ func TaskStatus() {
 }
 
 func ListClusters() {
-	ak, sk := "E101277-ak", "E101277-sk"
+	ak, sk := "Your Ak", "Your Sk"
 
 	eksClient, _ := eks.NewClient(ak, sk)
 
 	req := eks.ListClustersReq{
+		// 集群所使用的vpc的ID
 		VpcId: "d4ceb516-6def-11f0-a509-5672334e2706",
+		// 集群的名称(模糊搜素)或者ID
+		Keyword: "test-cluster",
 	}
 
 	response, err := eksClient.ListClusters(&req)
 	if err != nil {
 		fmt.Println(err)
-
+		return
 	}
-	fmt.Printf(">>> response: %+v", response)
 
-	fmt.Println(response.RequestId)
 	bytes, _ := json.Marshal(response)
 	fmt.Println(string(bytes))
 }
 
 func GetClusterEvents() {
-	ak, sk := "E101277-ak", "E101277-sk"
+	ak, sk := "Your Ak", "Your Sk"
 
 	eksClient, _ := eks.NewClient(ak, sk)
-
-	response, err := eksClient.GetClusterEvents("466603a8-32c4-4bd7-98c5-bb7b6b152e9e")
+	// 传集群的ID
+	response, err := eksClient.GetClusterEvents("b9ee61c2-87fa-4035-9ca5-a580980e3dde")
 	if err != nil {
 		fmt.Println(err)
-
+		return
 	}
-	fmt.Printf(">>> response: %+v", response)
 
-	fmt.Println(response.RequestId)
 	bytes, _ := json.Marshal(response)
 	fmt.Println(string(bytes))
 }
 
 func DeleteCluster() {
-	ak, sk := "E101277-ak", "E101277-sk"
+	ak, sk := "Your Ak", "Your Sk"
 
 	eksClient, _ := eks.NewClient(ak, sk)
-
+	// 传集群的ID
 	response, err := eksClient.DeleteCluster("4a5da7b9-0c3d-4dc3-96db-4c041c273d05")
 	if err != nil {
 		fmt.Println(err)
-
+		return
 	}
-	fmt.Printf(">>> response: %+v", response)
 
-	fmt.Println(response.RequestId)
 	bytes, _ := json.Marshal(response)
 	fmt.Println(string(bytes))
 }
