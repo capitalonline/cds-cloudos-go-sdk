f0a80313b0cd5654e53506d6f4d2e5c990e0bef3
boram-gong <18652730626@163.com>
2025-09-11 11:02:23 +0800

    change client

--- services/ecs/client.go
+++ services/ecs/client.go
@@ -25,6 +25,19 @@ const (
 	ebsRoute = "/ebs/v1"
 )
 
+type Client interface {
+	DescribeRegions() (*DescribeRegionsResult, error)
+	DescribeInstanceList(*DescribeInstanceListReq) (*DescribeInstanceListResult, error)
+	OperateInstance(*OperateInstanceReq) (*OperateInstanceResult, error)
+	ModifyInstanceName(*ModifyInstanceNameReq) (*ModifyInstanceNameResult, error)
+	DescribeInstance(*DescribeInstanceReq) (*DescribeInstanceResult, error)
+	DescribeTaskEvent(*DescribeTaskEventReq) (*DescribeTaskEventResult, error)
+	DescribeEcsFamilyInfo(*DescribeEcsFamilyInfoReq) (*DescribeEcsFamilyInfoResult, error)
+	ChangeInstanceConfigure(*ChangeInstanceConfigureReq) (*ChangeInstanceConfigureResult, error)
+
+	ExtendDisk(*ExtendDiskReq) (*ExtendDiskResult, error)
+}
+
 type client struct {
 	*cds.CdsClient
 	ecsRoute string
